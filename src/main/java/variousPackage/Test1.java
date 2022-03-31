65b1446ee08150617ac59252c13d89ed61b48cf9
Jhuma Feroza <jferz2190@gmail.com>
2022-03-31 12:18:50 -0500

    new commit

---
+++ src/main/java/variousPackage/Test1.java
@@ -0,0 +1,10 @@
+package variousPackage;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		System.out.println("Testone");
+		System.out.println("Testtwo");
+		System.out.println("Testhree");
+	}
+}
